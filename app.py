c8ef2b4c6269df79648818270c355dc1a7abad36
skillz <goslowbutneverstop@gmail.com>
2023-12-04 18:06:48 -0500

    starting to work on templates

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, url_for
+import sqlite3
+
+con = sqlite3.connect('paste.db')
+
+cursor = con.cursor()
+
+cursor.execute("CREATE TABLE IF NOT EXISTS paste (data TEXT,date DATE,id INTEGER)")
+
+con.commit()
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("base.html")
+
+if __name__=="__main__":
+    app.run(debug=True)
+
