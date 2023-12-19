0ed4404ed10380b1789c62c005e29f7011e5cf64
skillz <goslowbutneverstop@gmail.com>
2023-12-19 02:51:31 -0500

    Done with the desktop frontend

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for
 import sqlite3
+#import request
 
 con = sqlite3.connect('paste.db')
 
@@ -13,7 +14,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("paste.html", name = 'skillz')
+    return render_template("paste.html")
+
+@app.route("/paste", methods=['POST','GET'])
+def posting():
+    if request.method == 'POST':
+        print('executed')
+    if request.method == 'GET':
+        return render_template("paste.html")
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
