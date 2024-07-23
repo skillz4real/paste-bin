e3caca1763a8f717c8c3bcd79d1d10b94a6cd36a
TT <hahaha@lol.com>
2024-07-23 04:06:27 -0400

    Changed colors added animations

--- static/js/index.js
+++ static/js/index.js
@@ -4,3 +4,12 @@ for (let i = 0; i < darkReader.length; i++ ){
   darkReader[i].classList.add('hidden') 
 }
 
+const textarea = document.getElementById('paste');
+
+textarea.addEventListener('keydown', function(event) {
+  if (event)
+  event.preventDefault();
+
+  
+}
+
